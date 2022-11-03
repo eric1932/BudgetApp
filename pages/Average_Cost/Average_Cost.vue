d3a919ea3aa5e39ce5bc4ccc092e87b73090f88f
a37313506 <kamisama37313506@gmail.com>
2022-11-02 19:12:42 -0500

    加入了一张图片

---
+++ pages/Average_Cost/Average_Cost.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="static">
+		<image style="width: 400rpx; height: 200rpx;" src="../../static/logo.png" ></image>
+		
+	</div>
+		
+</template>
+
+<script>
+	export default {
+		}
+</script>
+
+<style lang = "css">
+	
+
+</style>
