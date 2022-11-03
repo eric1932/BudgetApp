0159ce1ae0c1d7ddd45df5d9793e76ffcdd39ff3
gong208 <60023638+gong208@users.noreply.github.com>
2022-11-02 19:10:19 -0500

    added settings

---
+++ pages/settings/language/language.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class = "languageContents">
+		当前语言：中文
+		<hr>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.languageContents {
+		text-align: center;
+		padding: 15px;
+		
+	}
+</style>
--- pages/settings/settings.vue
+++ pages/settings/settings.vue
@@ -1,6 +1,21 @@
 <template>
-	<view>
-		
+	<view class = "settingsContents">
+		<a href = "#/pages/settings/language/language" class = "settingItems">
+		<view class = "str">
+			语言
+		</view> 
+		</a><hr>
+		<a href = "#/pages/settings/feedBack/feedBack" class = "settingItems">
+		<view class = "str">
+			建议反馈
+		</view> 
+		</a> 
+		<hr>
+		<a href = "#/pages/settings/reminder/reminder" class = "settingItems">
+		<view class = "str">
+			每月交易提醒
+		</view>
+		</a><hr>
 	</view>
 </template>
 
@@ -12,11 +27,29 @@
 			}
 		},
 		methods: {
-			
+			test(){
+				this.$router.go(-1)
+			}
 		}
 	}
 </script>
 
 <style>
+	a:link {
+		color: black;
+		text-decoration: none;
+	},
+	a:visited {
+		color: black;
+		text-decoration: none;
+	}
+	.settingItems {
+		
+	},
+	.str {
+		padding: 10px;
+		background-color: aquamarine;
+	
+	}
 
 </style>
